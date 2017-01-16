0672bc11eb755b108d9b051dd465764e7f2e084d
zakonomernost <zakonomernost73@gmail.com>
2017-01-16 11:53:37 +0400

    Add files via upload

---
+++ laba4/ConsoleApplication3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task1 = new Task(() =>
+            {
+
+            });
+
+            var task2 = new Task(() =>
+            {
+
+            });
+
+            task1.ContinueWith(task =>
+            {
+                task2.Start();
+            });
+
+            task1.Start();
+        }
+    }
+}
